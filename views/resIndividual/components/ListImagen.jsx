2f7c8287e20600488f25ac61a649295c955a7591
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-06 21:59:08 -0500

    Create ListImagen.jsx
    Lista de iamgenes para vista res individual

---
+++ views/resIndividual/components/ListImagen.jsx
@@ -0,0 +1,88 @@
+import { FlatList, Image, StyleSheet, View, Dimensions, TouchableOpacity, Text } from 'react-native'
+import { useRef, useState } from 'react'
+import { SERVER_URL } from '@env'
+
+export default function ListImagen({ imageIds = ['00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000007', '00000000-0000-0000-0000-000000000005'] }) {
+  const screenWidth = Dimensions.get('window').width;  // Ancho de la pantalla
+  const [currentIndex, setCurrentIndex] = useState(0);  // Estado para el índice actual
+  const flatListRef = useRef(null);
+
+  // Función para moverse a la imagen siguiente
+  const handleNext = () => {
+    if (currentIndex < imageIds.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      flatListRef.current.scrollToIndex({ index: currentIndex + 1 });
+    }
+  };
+
+  // Función para moverse a la imagen anterior
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      flatListRef.current.scrollToIndex({ index: currentIndex - 1 });
+    }
+  };
+
+  const renderImage = ({ item }) => (
+    <View style={[styles.imageContainer, { width: screenWidth }]}>
+      <Image
+        style={styles.image}
+        source={{ uri: `${SERVER_URL}/imagen/id/${item}` }}
+        alt="Cow Image"
+      />
+    </View>
+  );
+
+  return (
+    <View style={styles.carouselContainer}>
+      <TouchableOpacity onPress={handlePrev} disabled={currentIndex === 0} style={styles.arrowButton}>
+        <Text style={[styles.arrowText, currentIndex === 0 && styles.disabledText]}>{"<"}</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        ref={flatListRef}
+        data={imageIds}
+        renderItem={renderImage}
+        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        snapToAlignment="center"
+        decelerationRate="normal"
+        scrollEnabled={false}
+      />
+
+      <TouchableOpacity onPress={handleNext} disabled={currentIndex === imageIds.length - 1} style={styles.arrowButton}>
+        <Text style={[styles.arrowText, currentIndex === imageIds.length - 1 && styles.disabledText]}>{">"}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  carouselContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    height: 180,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  arrowButton: {
+    padding: 10,
+  },
+  arrowText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  disabledText: {
+    color: 'gray'
+  },
+});
