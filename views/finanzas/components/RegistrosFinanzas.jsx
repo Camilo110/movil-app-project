7aca5be5ad52be080dc81bcb09872b4541acd7ae
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-24 19:39:11 -0500

    Update RegistrosFinanzas.jsx

--- views/finanzas/components/RegistrosFinanzas.jsx
+++ views/finanzas/components/RegistrosFinanzas.jsx
@@ -1,10 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import ResumenFinanzas from './ResumenFinanzas'
+import { getResumen, getAllTransaccion } from '../../../services/transaccion'
+import ItemFinanzas from './ItemFinanzas'
+import { useEffect, useState } from 'react'
 
 export default function RegistrosFinanzas() {
+
+  const [transacciones, setTransacciones] = useState([])
+  const [resumen, setResumen] = useState({Ingreso: 0, Egreso: 0, Balance: 0})
+
+  useEffect(() => {
+    fetchTransacciones()
+    fetchResumen()
+  }, [])
+
+  const fetchTransacciones = async () => {
+    const resp = await getAllTransaccion()
+    setTransacciones(resp)
+  }
+
+  const fetchResumen = async() => {
+    const resp = await getResumen()
+    setResumen(resp)
+  }
   return (
     <View>
-      <Text>RegistrosFinanzas</Text>
+      <ResumenFinanzas resumen={resumen}/>
+      <ScrollView>
+        {
+          transacciones.map((item) => (
+            <ItemFinanzas key={item.ID} item={item} />
+          ))
+        }
+      </ScrollView>
     </View>
   )
 }
