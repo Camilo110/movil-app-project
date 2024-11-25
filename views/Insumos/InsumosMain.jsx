162c3ba15da02fb000d680b8069cf5f24ae2723b
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-24 19:39:22 -0500

    Create InsumosMain.jsx

---
+++ views/Insumos/InsumosMain.jsx
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { InputText } from '../../components/Inputs'
+import { useState, useEffect } from 'react'
+import { ScrollView } from 'react-native'
+import {getInsumo} from '../../services/Insumo'
+import ItemRegistros from '../../components/itemRegistro'
+
+export default function InsumosMain() {
+  const [registros, setRegistros] = useState([])
+
+  useEffect(() => {
+    fetchRegistros()
+  }, [])
+
+  const fetchRegistros = async () => {
+    const registros = await getInsumo()
+    setRegistros(registros)
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Registros</Text>
+        <InputText styles={styles.input}placeholder="Fecha o Numero de Res" label={'Buscar Registros'} />
+
+      <ScrollView>
+        {
+          registros.map((registro) => {
+            return <ItemRegistros key={registro.ID} body={registro} />
+          })
+        }
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    width: '98%',
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  input: {
+    marginBottom: 10
+  }
+})
