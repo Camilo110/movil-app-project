4da87daee9952fb63673f0f91b858f84e0665fe1
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-24 19:39:02 -0500

    Update FormFinanzas.jsx

--- views/finanzas/components/FormFinanzas.jsx
+++ views/finanzas/components/FormFinanzas.jsx
@@ -13,26 +13,31 @@ export default function FormFinanzas() {
         <InputNumber
           label='Monto'
           placeholder='Monto'
+          value={0}
         />
 
         <InputDate
           label='Fecha'
           placeholder='Fecha'
+          value={''}
         />
 
         <InputText
           label={'descripcion'}
           placeholder='descripcion'
+          value={''}
         />
 
         <InputSelect
           label={'Cliente'}
           data={[{ label: 'Cliente1', value: 'Cliente1' }, { label: 'Cliente2', value: 'Cliente2' }, { label: 'Cliente3', value: 'Cliente3' }, { label: 'Cliente4', value: 'Cliente4' }]}
+          value={''}
         />
 
         <InputText
           label={'Observaciones'}
           placeholder='Observaciones'
+          value={''}
         />
 
         <View>
@@ -40,16 +45,19 @@ export default function FormFinanzas() {
             <InputSelect
               label={'Producto'}
               data={[{ label: 'Producto1', value: 'Producto1' }, { label: 'Producto2', value: 'Producto2' }, { label: 'Producto3', value: 'Producto3' }, { label: 'Producto4', value: 'Producto4' }]}
+              value={''}            
             />
 
             <InputNumber
               label='Cantidad'
               placeholder='Cantidad'
+              value={0}
             />
 
             <InputNumber
               label='Precio Unitario'
               placeholder='Precio Unitario'
+              value={0}
             />
           </View>
           
