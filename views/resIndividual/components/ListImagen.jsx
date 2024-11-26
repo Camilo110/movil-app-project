2f6795ea2d4c3e5aa30a402e77f5c262ab4d4d1c
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-25 20:56:33 -0500

    Update ListImagen.jsx

--- views/resIndividual/components/ListImagen.jsx
+++ views/resIndividual/components/ListImagen.jsx
@@ -1,27 +1,10 @@
 import { FlatList, Image, StyleSheet, View, Dimensions, TouchableOpacity, Text } from 'react-native'
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import { SERVER_URL } from '../../../config'
 
 export default function ListImagen({ imageIds = ['00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000007', '00000000-0000-0000-0000-000000000005'] }) {
-  const screenWidth = Dimensions.get('window').width;  // Ancho de la pantalla
-  const [currentIndex, setCurrentIndex] = useState(0);  // Estado para el índice actual
+  const screenWidth = Dimensions.get('window').width; 
   const flatListRef = useRef(null);
-
-  // Función para moverse a la imagen siguiente
-  const handleNext = () => {
-    if (currentIndex < imageIds.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-      flatListRef.current.scrollToIndex({ index: currentIndex + 1 });
-    }
-  };
-
-  // Función para moverse a la imagen anterior
-  const handlePrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-      flatListRef.current.scrollToIndex({ index: currentIndex - 1 });
-    }
-  };
 
   const renderImage = ({ item }) => (
     <View style={[styles.imageContainer, { width: screenWidth }]}>
