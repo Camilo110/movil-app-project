3793cc0c7e42aa5cd40f94643f89fd40d3861685
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-25 20:56:53 -0500

    Se añaden mejoras

--- views/resIndividual/resIndividual.jsx
+++ views/resIndividual/resIndividual.jsx
@@ -6,42 +6,19 @@ import { getResById, getHijos } from '../../services/res'
 import { NumeroRes } from '../../components/NumeroRes'
 import { daysToYearsandMonths } from '../../utils/DaysToYearsandMonths'
 import ListRegistros from './components/ListRegistros'
+
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 export default function ResIndividual({id}) {
 
   const [res, setRes] = useState({})
   const [numHijo, setNumHijo] = useState(0)
-
-  const [images, setImages] = useState([])
-
   const [loading, setLoading] = useState(true)
-
   const [linaje, setLinaje] = useState({})
-  const [servicios, setServicios] = useState([])
-  const [secados, setSecados] = useState([])
-  const [inseminaciones, setInseminaciones] = useState([])
-
-  const [imageMain, setImageMain] = useState({})
 
   useEffect(() => {
     fetchRes()
   }, [])
-
-  const fetchImages = async () => {
-    const images = await getImages(id)
-    setImages(images)
-    setImageMain(images[0])
-  }
-
-  const fetchServicios = async () => {
-    const servicios = await getServicioByIdRes(id)
-    setServicios(servicios)
-
-    const secado = await getSecadoByIdRes(id)
-    setSecados(secado)
-
-    const inseminacion = await getServicioWithInseminacionByIdRes(id)
-    setInseminaciones(inseminacion)
-  }
 
   const fetchRes = async () => {
     let listLinaje = []
@@ -77,19 +54,22 @@ export default function ResIndividual({id}) {
         loading ? <Text>Cargando...</Text> :
         <>
         <View style={styles.TopInfo}>
-          <View style={{flexDirection: 'row',}}>
+          <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
             <NumeroRes id = {id}/>
-            <Text>{res.Nombre}</Text>
+            <Text style={styles.title}>{res.Nombre}</Text>
           </View>
-          <View style={{flexDirection: 'row',}}>
+          <View style={{flexDirection: 'row', gap: 15}}>
             <Pressable>
-              <Text>Editar</Text>
+              <MaterialCommunityIcons name="image-plus" size={24} color="black" />
             </Pressable>
             <Pressable>
-              <Text>Eliminar</Text>
+              <MaterialCommunityIcons name="circle-edit-outline" size={24} color="black" />
             </Pressable>
             <Pressable>
-              <Text>Imagen</Text>
+              <MaterialCommunityIcons name="skull-crossbones" size={24} color="black" />
+            </Pressable>
+            <Pressable>
+              <MaterialCommunityIcons name="cash-check" size={24} color="black" />
             </Pressable>
           </View>
         </View> 
@@ -99,13 +79,17 @@ export default function ResIndividual({id}) {
           <View style={styles.infoExt}>
             <View style={styles.info}>
               <Text style={{fontWeight: 'bold'}}>Información</Text>
-              <Text><Text style={{fontWeight: 'bold'}}>Número de partos:</Text> {res.NumeroCrias}</Text>
+              {
+                res.Sexo === 'F' ?
+                <Text><Text style={{fontWeight: 'bold'}}>Número de partos:</Text> {res.NumeroCrias}</Text>
+                :
+                <Text><Text style={{fontWeight: 'bold'}}>Cantidad de Hijos:</Text> {numHijo}</Text>
+              }
               <Text><Text style={{fontWeight: 'bold'}}>Ubicación:</Text> {res.FincaNombre}</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Edad:</Text> {daysToYearsandMonths(res.Edad)}</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Estado:</Text> {res.Estado}</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Peso al Nacer:</Text> {res.PesoNacimiento} Kg</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Peso Actual:</Text> {res.PesoActual} Kg</Text>
-              <Text><Text style={{fontWeight: 'bold'}}>Cantidad de Hijos:</Text> {numHijo}</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Tipo:</Text> {res.Tipo}</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Sexo:</Text> {res.Sexo}</Text>
               <Text><Text style={{fontWeight: 'bold'}}>Raza:</Text> {res.Raza}</Text>
@@ -118,6 +102,7 @@ export default function ResIndividual({id}) {
             Linaje
           </Text>
           <ListLinaje listaLinaje = {linaje}/>
+          
           <Text>
             Registros
           </Text>
@@ -134,12 +119,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     width: '100%',
-    padding: 3,
+    padding: 3
   },
   title: {
-    fontSize: 24,
+    marginHorizontal: 10,
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 20,
     color: 'black',
   },
   TopInfo: {
@@ -147,6 +132,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 5,
+    marginBottom: 10,
+    padding:10,
+    paddingHorizontal: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
   },
@@ -160,6 +148,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
     padding: 10,
     borderRadius: 15,
-
   }
 })
