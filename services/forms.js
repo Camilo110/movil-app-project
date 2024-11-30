5f263bc7beacdecb41acf293df00094bec508989
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-29 19:43:12 -0500

    Ajustes de rutas

--- services/forms.js
+++ services/forms.js
@@ -4,12 +4,12 @@ export const getResModal = async () => {
   const resp= await fetch(`${SERVER_URL}/finca`);
   console.log("CUIDADO")
   const {body} = await resp.json();
-  const fincas = body.map(({ID, Nombre}) => ({value: ID, label: Nombre}));
+  const fincas = body.map(({ID, Nombre}) => ({key: ID, label: Nombre}));
 
   const response= await fetch(`${SERVER_URL}/res`);
   const {body: reses} = await response.json();
-  const madres = reses.filter(({Sexo}) => Sexo === 'F').map(({ID, Nombre}) => ({value: ID, label: Nombre}))
-  const padres = reses.filter(({Sexo}) => Sexo === 'M').map(({ID, Nombre}) => ({value: ID, label: Nombre}))
+  const madres = reses.filter(({Sexo}) => Sexo === 'F').map(({ID, Nombre}) => ({key: ID, label: Nombre}))
+  const padres = reses.filter(({Sexo}) => Sexo === 'M').map(({ID, Nombre}) => ({key: ID, label: Nombre}))
 
   return {fincas, madres, padres}
  }
@@ -25,20 +25,20 @@ export const getResModal = async () => {
  export const getServiciosModal = async () => {
   const response = await fetch(`${SERVER_URL}/res`);
   const {body} = await response.json();
-  const reses = body.map(({ID, Nombre}) => ({ID, value: Nombre}))
+  const reses = body.map(({ID, Nombre}) => ({key: ID, value: Nombre}))
   return reses
  }
 
  export const getProveedor = async () => {
   const response = await fetch(`${SERVER_URL}/proveedor`);
   const {body} = await response.json();
-  const proveedores = body.map(({ID, Nombre}) => ({ID, value: Nombre}))
+  const proveedores = body.map(({ID, Nombre}) => ({key: ID, value: Nombre}))
   return proveedores
  }
 
  export const getCliente = async () => {
   const response = await fetch(`${SERVER_URL}/cliente`);
   const {body} = await response.json();
-  const clientes = body.map(({ID, Nombre}) => ({ID, value: Nombre}))
+  const clientes = body.map(({ID, Nombre}) => ({key: ID, value: Nombre}))
   return clientes
  }
--- services/images.js
+++ services/images.js
@@ -1,30 +1,57 @@
+import { SERVER_URL } from "../config";
+
 export const getImages = async (id) => {
-  const response = await fetch(`http://localhost:4000/imagen/${id}`);
+  const response = await fetch(`${SERVER_URL}/imagen/${id}`);
   const {body} = await response.json()
   return body
 }
 
 
-export const uploadImage = async (id, files, key) => {
-  const formData = new FormData()
-  for (let i = 0; i < files.length; i++) {
-      formData.append(key, files[i])
-  }
+// export const uploadImage = async (id, files, key) => {
+//   const formData = new FormData()
+//   for (let i = 0; i < files.length; i++) {
+//       formData.append(key, files[i])
+//   }
+//   formData.append('resID', id)
+
+//   const response = await fetch(`${SERVER_URL}/imagen`, {
+//       method: 'POST',
+//       body: formData,
+//   });
+//   const resp = await response.json()
+//   console.log(resp)
+//   if (resp.status == 200) {
+//       return 'OK'
+//   }
+// }
+
+export const uploadImage = async (imageUri, id) => {
+  const formData = new FormData();
+  formData.append('images', {
+    uri: imageUri,
+    name: 'photo.jpg',
+    type: 'image/jpeg',
+  });
   formData.append('resID', id)
 
-  const response = await fetch('http://localhost:4000/imagen', {
+  try {
+   const response = await fetch(`${SERVER_URL}/imagen`, {
       method: 'POST',
       body: formData,
-  });
-  const resp = await response.json()
-  console.log(resp)
-  if (resp.status == 200) {
-      return 'OK'
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    const data = await response.json();
+    console.log('Imagen subida exitosamente:', data);
+  } catch (error) {
+    console.error('Error al subir la imagen:', error);
   }
-}
+};
+
 
 export const deleteImage = async (id) => {
-  const response = await fetch(`http://localhost:4000/imagen/${id}`, {
+  const response = await fetch(`${SERVER_URL}/imagen/${id}`, {
       method: 'DELETE',
   });
   const resp = await response.json()
