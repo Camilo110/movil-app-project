492aa0e334074d184abbbc6eef30b74c7a30e97a
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-29 19:42:48 -0500

    Se mejora el componente para que se mas flexible

--- components/itemRegistro.jsx
+++ components/itemRegistro.jsx
@@ -1,28 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { Pressable } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-export default function ItemRegistro({ body: { ID, Numero, ResID, listInsumos, Tipo, ...body }, onDelete, onEdit }) {
+export default function ItemRegistro({ data, keyTitle1, keyTitle2, restKeys, labels, onDelete, onEdit }) {
   return (
-    <Pressable style={styles.tarjetaRegistro}>
-      <View style={styles.numero}>
-        <Text style={styles.numeroText}>{Tipo}</Text>
-        <Text style={styles.numeroText}>N° {Numero}</Text>
-      </View>
-      <View style={styles.contenido}>
-        {Object.keys(body).map((key) => (
-          <Text key={key} style={styles.text}>
-            {body[key] && <><Text style={{fontWeight: 'bold'}}>{key}:</Text> {body[key]}</>}
-          </Text>
-        ))}
+    <Pressable 
+      onPress={() =>onEdit && onEdit(data)} 
+      style={styles.tarjetaRegistro}>
 
-        {listInsumos && (
-          <Text style={styles.text}><Text style={{fontWeight: 'bold'}}>Lista Insumos:</Text> {listInsumos}</Text>
-        )}
+      <View style={styles.content}>
+        <View>
+          <View style={styles.titles}>
+            <Text style={styles.title}>{data[keyTitle1]}</Text>
+            <Text style={styles.title}>{keyTitle1 && data[keyTitle2]}</Text>
+          </View>
+
+          <View style={styles.contenido}>
+            {restKeys.map((key, index) => (
+              <Text key={data.ID + key} style={styles.text}>
+                {data[key] && (
+                  <>
+                    <Text style={{ fontWeight: 'bold' }}>{labels[index]}:</Text> {data[key]}
+                  </>
+                )}
+              </Text>
+            ))}
+          </View>
+        </View>
+
+        {
+          onDelete &&
+          <Pressable 
+            style={[styles.deleteButton, { backgroundColor: '#ff4d4d' }]} 
+            onPress={() => onDelete(data.ID)}>
+            <MaterialCommunityIcons name="delete-forever" size={24} color="white" />
+          </Pressable>
+        }
       </View>
-      
     </Pressable>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -33,19 +50,36 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#ddd',
+    marginVertical: 3,
   },
-  numero: {
+  content: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    paddingRight: 30,
+  },
+  titles: {
     marginBottom: 8,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  numeroText: {
+  title: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  contenido: {
+    marginBottom: 8,
   },
   text: {
     fontSize: 16,
     color: '#333',
     marginBottom: 4,
   },
-})
+  deleteButton: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    paddingVertical: 2,
+    paddingHorizontal: 1,
+    borderRadius: 3,
+  },
+});
