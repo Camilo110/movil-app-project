434b3d161293c69c67f42c6e5cce3051ff98535a
Camilo Urrea <luissepulveda110@gmail.com>
2024-11-29 19:44:16 -0500

    Update ItemRegistros.jsx

--- views/produccion/components/ItemRegistros.jsx
+++ views/produccion/components/ItemRegistros.jsx
@@ -1,21 +1,50 @@
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
-import React from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import ModalConfirmacion from '../../../components/ModalConfirmacion'
+import { DeleteProduccionIndividual } from '../../../services/produccion'
 
-export default function ItemRegistros({body: {Fecha, Tipo, Cantidad, ResNombre}}) {
+export default function ItemRegistros({body: {Fecha, Tipo, Cantidad, ResNombre, ...body}, fetch}) {
+
+  const onDelete = async () => {
+    console.log(body.ID)
+    DeleteProduccionIndividual(body.ID)
+    fetch()
+  }
+
   return (
     <Pressable >
       <View style={styles.container}>
         
-      <Image
-        source={require('../../../assets/favicon.png')}
-        style={styles.image}
-      />
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>{Cantidad}</Text>
-        <Text style={styles.text}>{Fecha}</Text>
-        <Text style={styles.text}>{ResNombre}</Text>
-      </View>
+        {
+          Tipo === 'Leche' ? 
+          <Image
+            source={require('../../../assets/img/milk.png')}
+            style={styles.image}
+          /> :
+          <Image
+            source={require('../../../assets/img/cow.png')}
+            style={styles.image}
+          />
+        }
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{Cantidad} {Tipo === 'Leche' ? 'Lts' : 'Kg'}</Text>
+          <Text style={styles.text}>{Fecha}</Text>
+          <Text style={styles.text}>{ResNombre}</Text>
+
+          <ModalConfirmacion
+            title="Eliminar Registro"
+            description="¿Estas seguro de eliminar este registro?"
+            onConfirm={onDelete}
+          >
+            <View 
+              style={[styles.deleteButton, { backgroundColor: '#ff4d4d' }]}
+            >
+              <MaterialCommunityIcons name="delete-forever" size={24} color="white" />
+            </View>
+          </ModalConfirmacion>
+          
         </View>
+      </View>
     </Pressable>
   )
 }
@@ -24,10 +53,13 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#E8F6F3',
     padding: 10,
-    marginVertical: 5,
+    marginVertical: 2,
     borderRadius: 8,
+    gap: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    backgroundColor: '#fff',
   },
   image: {
     width: 50,
@@ -41,4 +73,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#000', // Texto negro
   },
+  deleteButton: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    paddingVertical: 2,
+    paddingHorizontal: 1,
+    borderRadius: 3,
+  }
 })
